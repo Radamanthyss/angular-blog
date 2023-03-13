c2ce4f498517ae067365f3b929d3c06879ce3cbb
BRUNO <92279785+Radamanthyss@users.noreply.github.com>
2023-03-13 17:00:24 -0300

    geração de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './component/menu/menu.component';
+import { TitleComponent } from './component/title/title.component';
+import { DestaqueComponent } from './component/destaque/destaque.component';
+import { CardComponent } from './component/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    TitleComponent,
+    DestaqueComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
